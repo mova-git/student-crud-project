b61c0b3e660070b5d296cb9d10f7d9ed051f26c5
mova-git <mauriya1999@gmail.com>
2026-07-14 13:41:12 +0530

    changes 3.4

--- scripts/report.py
+++ scripts/report.py
@@ -2,23 +2,33 @@ import pandas as pd
 import mysql.connector
 import os
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="studentdb"
-)
+try:
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="studentdb"
+    )
 
-query = "SELECT * FROM students"
-df = pd.read_sql(query, conn)
+    print("Connected to MySQL")
 
-output_dir = r"D:\login-db-project\reports"
-os.makedirs("output_dir", exist_ok=True)
+    query = "SELECT * FROM students"
+    df = pd.read_sql(query, conn)
 
-output_file = os.path.join(output_dir, "Student_Report.xlsx")
-df.to_excel("reports/Student_Report.xlsx", index=False)
+    print(df)
 
-print("Excel Report Generated Successfully!")
-print(f"Location: {output_file}")
+    output_dir = r"D:\login-db-project\reports"
+    os.makedirs(output_dir, exist_ok=True)
 
-conn.close()
+    output_file = os.path.join(output_dir, "Student_Report.xlsx")
+
+    df.to_excel(output_file, index=False)
+
+    print("Excel Report Generated Successfully!")
+    print("Saved at:", output_file)
+
+    conn.close()
+
+except Exception as e:
+    print("ERROR:")
+    print(e)
