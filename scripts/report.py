77014a021ce9a2d6a05efa37f7b2ee6406230ca3
mova-git <mauriya1999@gmail.com>
2026-07-14 13:30:43 +0530

    changes 3.2

--- scripts/report.py
+++ scripts/report.py
@@ -10,13 +10,15 @@ conn = mysql.connector.connect(
 )
 
 query = "SELECT * FROM students"
-
 df = pd.read_sql(query, conn)
 
-os.makedirs("reports", exist_ok=True)
+output_dir = r"D:\login-db-project\reports"
+os.makedirs("output_dir", exist_ok=True)
 
+output_file = os.path.join(output_dir, "Student_Report.xlsx")
 df.to_excel("reports/Student_Report.xlsx", index=False)
 
 print("Excel Report Generated Successfully!")
+print(f"Location: {output_file}")
 
 conn.close()
